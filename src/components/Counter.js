a1a80576de8769b7f8ab065789683fb8b23f978e
shalini <s.rathore43@gmail.com>
2024-10-30 11:17:22 +0530

    before vacation

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -13,12 +13,18 @@ export default function Counter() {
     x--;
     setcount(x)
   }
+  let cnt = <div className="countergroup">{count>0?<input type="button" value="-" onClick={()=>decrement()}>
+
+  </input>:<input type="button" value="-" disabled></input>}
+  <div>{count}</div>
+  {count<5?<input type="button" value="+" onClick={()=>increment()}></input>:
+  <input type="button" value="+" disabled></input>}
+  </div>
 return(
   <>
-  {count>0?<input type="button" value="-" onClick={()=>decrement()}></input>:<input type="button" value="-" disabled></input>}
-  <div>{count}</div>
-  {count<5?<input type="button" value="+" onClick={()=>increment()}></input>:<input type="button" value="+" disabled></input>}
   
+  {count<=0?<input type="button" value="Add to Cart" className="btn btnaddcart" 
+  onClick={()=>setcount(1)}/>:cnt}
   </>
 )
 
